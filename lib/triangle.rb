55277d028f51cba82262ac8b92df5ac43b1f638b
Bloom <bloomtauta@gmail.com>
2023-02-20 22:51:56 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a      
+    @b = b      
+    @c = c 
+  end
+
+  def valid_num?
+    [@a, @b, @c].all? { |num| num > 0}
+  end
+
+  def valid_triangle?
+    @a + @b > @c && @b + @c > @a && @a + @c > @b
+  end
+
+  def kind
+    try_error 
+    if @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @b == @c ||  @a == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  def try_error
+    raise TriangleError unless valid_num? && valid_triangle?
+  end
+  
+  class TriangleError < StandardError
+  end
 end
